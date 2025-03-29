674e71160d321ad913fb0de73c8abbe8ba0d3dd3
VikasRajput-18 <vikasvikas98809@gmail.com>
2025-03-29 23:23:01 +0530

    setup folder structure and auth form ui

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monotSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Prep AI",
+  description: "An AI-Powered platform for preparing for mock interviews",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${monotSans.className} antialiased pattern`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ components/FormField.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { FormItem, FormLabel, FormControl, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: "email" | "text" | "password" | "file";
+}
+
+const FormField = <T extends FieldValues>({
+  name,
+  control,
+  label,
+  placeholder,
+  type = "text",
+}: FormFieldProps<T>) => {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className="label">{label}</FormLabel>
+          <FormControl>
+            <Input
+              className="input"
+              type={type}
+              placeholder={placeholder}
+              {...field}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default FormField;
